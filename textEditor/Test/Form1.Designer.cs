476818ea197ba8c471dcd4444ff8eb7fb140bff9
Gunay <gunayaa@code.edu.az>
2018-07-07 16:56:27 +0400

    C# Text editor

---
+++ textEditor/Test/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbTest = new System.Windows.Forms.RichTextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbTest
+            // 
+            this.rtbTest.Location = new System.Drawing.Point(12, 12);
+            this.rtbTest.Name = "rtbTest";
+            this.rtbTest.Size = new System.Drawing.Size(295, 403);
+            this.rtbTest.TabIndex = 0;
+            this.rtbTest.Text = "";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(325, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(152, 47);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "SELECT";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(325, 121);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(152, 49);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(331, 65);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 13);
+            this.lblFileName.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(325, 232);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(152, 49);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 427);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.rtbTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTest;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ textEditor/Test/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private string SelectedFile;
+        public Form1()
+        {
+            InitializeComponent();
+            Test1();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Test1()
+        {
+
+            string path = @"C:\Users\pc\Desktop\test";
+            //Directory.CreateDirectory(@"C:\Users\pc\Desktop\test");
+            //Directory.Delete(path, true);
+            //File.Create(path + @"\text.txt");
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            this.SelectedFile = openFile.FileName;
+            ReadFile();
+        }
+        private void ReadFile()
+        {
+            string type = this.SelectedFile.Split('.')[1];
+            if (type == "txt")
+            {
+                lblFileName.Text = openFile.FileName;
+                using (StreamReader sr=new StreamReader(this.SelectedFile))
+                {
+                    string line = sr.ReadToEnd();
+                    rtbTest.Text = line;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ancag txt file sece bilersiniz.");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (this.SelectedFile != null)
+            {
+                using (StreamWriter sw = new StreamWriter(this.SelectedFile))
+                {
+                    sw.WriteLine(rtbTest.Text);
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (this.SelectedFile != null)
+            {
+                this.SelectedFile = null;
+                lblFileName.Text = "";
+                rtbTest.Text = "";
+            }
+        }
+    }
+}
